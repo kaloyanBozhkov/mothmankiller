b3d5ed454917e55ae46523c10c158a85df6e1be0
Kaloyan D. Bozhkov <kaloyan@bozhkov.com>
2024-06-12 09:55:06 +0300

    .

--- src/pages/api/sheets/sync-events.ts
+++ src/pages/api/sheets/sync-events.ts
@@ -40,10 +40,11 @@ export default async function syncEventsServerless(
       },
     );
 
-  console.log(1, JSON.stringify(req));
+  console.log(req.method);
+  console.log(req.body);
+  console.log(req.headers);
 
   if (req.method !== "POST" || !req.body) return oops();
-  console.log(2);
 
   try {
     const input = await req.body.getReader().read(),
@@ -51,10 +52,8 @@ export default async function syncEventsServerless(
       string = decoder.decode(input.value),
       payload = JSON.parse(string) as SyncCurrenciesPayload,
       { secret, events } = SyncEventsSchema.parse(payload);
-    console.log(3);
 
     if (!secret || secret !== process.env.SENSITIVE_CRUD_SECRET) return oops();
-    console.log(4);
 
     event.waitUntil(
       (async () => {
