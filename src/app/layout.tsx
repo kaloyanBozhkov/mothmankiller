939d278937a297a58b566ba4fe94abafb16675d7
Kaloyan D. Bozhkov <kaloyan@bozhkov.com>
2024-06-14 14:50:26 +0300

    add ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { TRPCReactProvider } from "@/trpc/react";
 
 import AOS from "@/app/_components/next-components/AOS.next";
 import CSSVaraibles from "../../tailwind/plugins";
-
+import { GoogleAnalytics } from "@next/third-parties/google";
 export const metadata = {
   title: "Mothmankiller",
   description: "Bringing back the 00s sound. SOFIA | BULGARIA",
@@ -23,6 +23,7 @@ export default function RootLayout({
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <style type="text/css">{CSSVaraibles}</style>
+        <GoogleAnalytics gaId="G-9XF47FL1PM" />
       </head>
       <body>
         <TRPCReactProvider>
